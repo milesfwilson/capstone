179f51e97e009648fbb132a98573286bda95849a
Kody Dudley <kodymdudley@gmail.com>
2020-12-11 15:03:58 -0700

    heroku5

--- capstone.client/vue.config.js
+++ capstone.client/vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   // publicPath: '/',
   outputDir: '../capstone.server/client',
-
-  productionSourceMap: false
+  productionSourceMap: false,
+  transpileDependencies: ['@bcwdev/auth0provider-client']
 }
